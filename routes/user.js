0ab78ca871a0336edef920e24871997703c1af37
Lila Guillermic <lila.guillermic@gmail.com>
2020-06-06 21:35:43 +0200

    fix stripe

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const formidableMiddleware = require("express-formidable");
-router.use(formidableMiddleware());
+const express = require("express")
+const router = express.Router()
+const formidableMiddleware = require("express-formidable")
+router.use(formidableMiddleware())
 
-const User = require("../models/User");
-const cryptPassword = require("../utils/cryptPassword");
-const uncryptPassword = require("../utils/uncryptPassword");
+const User = require("../models/User")
+const cryptPassword = require("../utils/cryptPassword")
+const uncryptPassword = require("../utils/uncryptPassword")
 
 router.post("/user/signup", async (req, res) => {
   if (req.fields.username && req.fields.email && req.fields.password) {
-    const searchEmail = await User.findOne({ email: req.fields.email });
+    const searchEmail = await User.findOne({ email: req.fields.email })
 
     if (!searchEmail) {
-      const crypted = cryptPassword(req.fields.password);
+      const crypted = cryptPassword(req.fields.password)
 
       try {
         const newUser = new User({
@@ -22,41 +22,41 @@ router.post("/user/signup", async (req, res) => {
           salt: crypted.salt,
           account: {
             username: req.fields.username,
-            phone: req.fields.phone
-          }
-        });
-        await newUser.save();
+            phone: req.fields.phone,
+          },
+        })
+        await newUser.save()
         res.json({
           token: newUser.token,
           email: newUser.email,
-          username: newUser.account.username
-        });
+          username: newUser.account.username,
+        })
       } catch (error) {
-        res.json({ message: error.message });
+        res.json({ message: error.message })
       }
     } else {
-      res.json({ message: "User already exists" });
+      res.json({ message: "User already exists" })
     }
   } else {
     res.json({
-      message: "Missing information"
-    });
+      message: "Informations manquantes",
+    })
   }
-});
+})
 
 router.post("/user/login", async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.fields.email });
+    const user = await User.findOne({ email: req.fields.email })
     if (user) {
-      const uncrypted = uncryptPassword(req.fields.password, user);
+      const uncrypted = uncryptPassword(req.fields.password, user)
 
-      res.json(uncrypted);
+      res.json(uncrypted)
     } else {
-      return res.status(400).json("User not found");
+      return res.status(400).json("User not found")
     }
   } catch (error) {
-    return res.status(400).json({ message: "An error occurred" });
+    return res.status(400).json({ message: "An error occurred" })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
